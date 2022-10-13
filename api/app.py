272d2610b0f0b46795bc8fd00972e1a249283bdd
Le Kang Wang <lekang.wang@yahoo.ca>
2022-10-13 15:04:55 -0400

    updated User db model

--- api/app.py
+++ api/app.py
@@ -1,31 +1,45 @@
+import os
+
+# Core libraries
 from flask import Flask, flash
 from flask_sqlalchemy import SQLAlchemy
-import os
+from flask_login import LoginManager
+
+# Services
 from search_service import search_service
+
 # Utility libraries
 from dotenv import load_dotenv
 import bcrypt
 
+# Backend setup
 load_dotenv()
 app = Flask(__name__)
+login_manager = LoginManager()
 
-# Postgres Database Setup
+# Postgres db setup
 app.config["SECRET_KEY"] = os.environ["DATABASE_SECRET"]
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-app.register_blueprint(search_service)
-
 # Import models
 from models.users import User
 
+db.create_all()
+db.session.commit()
+login_manager.init_app(app)
+
+# Services registration
+app.register_blueprint(search_service)
+
 @app.route("/")
 def test():
-    new_user = User("kangkanglw", "123@sesamestreet.ca", bcrypt.hashpw("12345".encode("utf-8"), bcrypt.gensalt()))
+    new_user = User("123@sesamestreet.ca", "ass", "america", "2000", "200", "100", "50")
     db.session.add(new_user)
     db.session.commit()
     flash("New User Added")
+    print(new_user.check_password("ass"))
     return (f"User created!: {str(new_user)}", 200)
 
 if __name__ == "__main__":
--- api/models/users.py
+++ api/models/users.py
@@ -1,17 +1,33 @@
 from app import db
+from flask_login import UserMixin
+import bcrypt
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50))
     email = db.Column(db.String(50))
-    password = db.Column(db.String(50))
+    password = db.Column(db.String(200))
+    timezone = db.Column(db.String(50))
+    calorie_limit = db.Column(db.Float(50))
+    protein_limit = db.Column(db.Float(50))
+    fat_limit = db.Column(db.Float(50))
+    carb_limit = db.Column(db.Float(50))
 
-    def __init__(self, username, email, password):
-        self.username = username
+    def __init__(self, email, password, timezone, calorie_limit, protein_limit, fat_limit, carb_limit):
         self.email = email
-        self.password = password
+        self.password = self.salt_password(password).decode("utf-8")
+        self.timezone = timezone
+        self.calorie_limit = calorie_limit
+        self.protein_limit = protein_limit
+        self.fat_limit = fat_limit
+        self.carb_limit = carb_limit
+
+    def salt_password(self, password):
+        return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
 
     def __repr__(self):
-        return f"username: {self.username}, email: {self.email}, salted password: {self.password}"
+        return f"email: {self.email}, salted password: {self.password}"
