6263a80c95a99499403e3139e5ca738ff28531aa
Le Kang Wang <lekang.wang@yahoo.ca>
2022-10-16 22:40:55 -0400

    enabled custom styling

--- screens/util/Typography.js
+++ screens/util/Typography.js
@@ -1,28 +1,72 @@
 import { palette } from "./Palette";
 import { Text, StyleSheet } from "react-native";
 
-export function P({ children }) {
-  return <Text style={styles.p}>{children}</Text>;
+export function P({ children, style }) {
+  return (
+    <Text
+      style={style instanceof Array ? [styles.p, ...style] : [styles.p, style]}
+    >
+      {children}
+    </Text>
+  );
 }
 
-export function H1({ children }) {
-  return <Text style={styles.h1}>{children}</Text>;
+export function H1({ children, style }) {
+  return (
+    <Text
+      style={
+        style instanceof Array ? [styles.h1, ...style] : [styles.h1, style]
+      }
+    >
+      {children}
+    </Text>
+  );
 }
 
-export function H2({ children }) {
-  return <Text style={styles.h2}>{children}</Text>;
+export function H2({ children, style }) {
+  return (
+    <Text
+      style={
+        style instanceof Array ? [styles.h2, ...style] : [styles.h2, style]
+      }
+    ></Text>
+  );
 }
 
-export function H3({ children }) {
-  return <Text style={styles.h3}>{children}</Text>;
+export function H3({ children, style }) {
+  return (
+    <Text
+      style={
+        style instanceof Array ? [styles.h3, ...style] : [styles.h3, style]
+      }
+    >
+      {children}
+    </Text>
+  );
 }
 
-export function H4({ children }) {
-  return <Text style={styles.h4}>{children}</Text>;
+export function H4({ children, style }) {
+  return (
+    <Text
+      style={
+        style instanceof Array ? [styles.h4, ...style] : [styles.h4, style]
+      }
+    >
+      {children}
+    </Text>
+  );
 }
 
-export function H5({ children }) {
-  return <Text style={styles.h5}>{children}</Text>;
+export function H5({ children, style }) {
+  return (
+    <Text
+      style={
+        style instanceof Array ? [styles.h5, ...style] : [styles.h5, style]
+      }
+    >
+      {children}
+    </Text>
+  );
 }
 
 const styles = StyleSheet.create({
